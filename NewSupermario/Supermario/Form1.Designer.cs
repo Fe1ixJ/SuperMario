3c1d36104c2179b422f2ed39405d7780903c6895
Fe1ixJ <66786588+Fe1ixJ@users.noreply.github.com>
2022-03-11 21:02:53 +0100

    Add files via upload

---
+++ NewSupermario/Supermario/Form1.Designer.cs
@@ -0,0 +1,341 @@
+﻿namespace Supermario
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.Bakgrund = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.Marken1 = new System.Windows.Forms.PictureBox();
+            this.Marken2 = new System.Windows.Forms.PictureBox();
+            this.Marken3 = new System.Windows.Forms.PictureBox();
+            this.Marken4 = new System.Windows.Forms.PictureBox();
+            this.TestBox = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.Mario = new System.Windows.Forms.PictureBox();
+            this.TestBox2 = new System.Windows.Forms.PictureBox();
+            this.Marken5 = new System.Windows.Forms.PictureBox();
+            this.pictureBox6 = new System.Windows.Forms.PictureBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.Bakgrund)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Marken1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Marken2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Marken3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Marken4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.TestBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Mario)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.TestBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Marken5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // Bakgrund
+            // 
+            this.Bakgrund.AccessibleRole = System.Windows.Forms.AccessibleRole.None;
+            this.Bakgrund.Image = ((System.Drawing.Image)(resources.GetObject("Bakgrund.Image")));
+            this.Bakgrund.Location = new System.Drawing.Point(0, 0);
+            this.Bakgrund.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Bakgrund.Name = "Bakgrund";
+            this.Bakgrund.Size = new System.Drawing.Size(14402, 1280);
+            this.Bakgrund.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Bakgrund.TabIndex = 0;
+            this.Bakgrund.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Supermario.Properties.Resources.Marken;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 1195);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(73, 85);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::Supermario.Properties.Resources.Marken;
+            this.pictureBox2.Location = new System.Drawing.Point(73, 1195);
+            this.pictureBox2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(73, 85);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 2;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = global::Supermario.Properties.Resources.Marken;
+            this.pictureBox3.Location = new System.Drawing.Point(146, 1195);
+            this.pictureBox3.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(73, 85);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 3;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Image = global::Supermario.Properties.Resources.Marken;
+            this.pictureBox4.Location = new System.Drawing.Point(219, 1195);
+            this.pictureBox4.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(73, 85);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox4.TabIndex = 4;
+            this.pictureBox4.TabStop = false;
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.Image = global::Supermario.Properties.Resources.Marken;
+            this.pictureBox5.Location = new System.Drawing.Point(293, 1195);
+            this.pictureBox5.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(73, 85);
+            this.pictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox5.TabIndex = 5;
+            this.pictureBox5.TabStop = false;
+            // 
+            // Marken1
+            // 
+            this.Marken1.Image = global::Supermario.Properties.Resources.Marken;
+            this.Marken1.Location = new System.Drawing.Point(0, 1109);
+            this.Marken1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Marken1.Name = "Marken1";
+            this.Marken1.Size = new System.Drawing.Size(73, 85);
+            this.Marken1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Marken1.TabIndex = 6;
+            this.Marken1.TabStop = false;
+            // 
+            // Marken2
+            // 
+            this.Marken2.Image = global::Supermario.Properties.Resources.Marken;
+            this.Marken2.Location = new System.Drawing.Point(73, 1109);
+            this.Marken2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Marken2.Name = "Marken2";
+            this.Marken2.Size = new System.Drawing.Size(73, 85);
+            this.Marken2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Marken2.TabIndex = 7;
+            this.Marken2.TabStop = false;
+            // 
+            // Marken3
+            // 
+            this.Marken3.Image = global::Supermario.Properties.Resources.Marken;
+            this.Marken3.Location = new System.Drawing.Point(146, 1109);
+            this.Marken3.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Marken3.Name = "Marken3";
+            this.Marken3.Size = new System.Drawing.Size(73, 85);
+            this.Marken3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Marken3.TabIndex = 8;
+            this.Marken3.TabStop = false;
+            // 
+            // Marken4
+            // 
+            this.Marken4.Image = global::Supermario.Properties.Resources.Marken;
+            this.Marken4.Location = new System.Drawing.Point(219, 1109);
+            this.Marken4.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Marken4.Name = "Marken4";
+            this.Marken4.Size = new System.Drawing.Size(73, 85);
+            this.Marken4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Marken4.TabIndex = 9;
+            this.Marken4.TabStop = false;
+            // 
+            // TestBox
+            // 
+            this.TestBox.Image = global::Supermario.Properties.Resources.Marken;
+            this.TestBox.Location = new System.Drawing.Point(362, 963);
+            this.TestBox.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.TestBox.Name = "TestBox";
+            this.TestBox.Size = new System.Drawing.Size(73, 85);
+            this.TestBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.TestBox.TabIndex = 10;
+            this.TestBox.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Mario
+            // 
+            this.Mario.Image = global::Supermario.Properties.Resources.small_mario;
+            this.Mario.Location = new System.Drawing.Point(146, 683);
+            this.Mario.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Mario.Name = "Mario";
+            this.Mario.Size = new System.Drawing.Size(73, 85);
+            this.Mario.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Mario.TabIndex = 11;
+            this.Mario.TabStop = false;
+            // 
+            // TestBox2
+            // 
+            this.TestBox2.Image = global::Supermario.Properties.Resources.Marken;
+            this.TestBox2.Location = new System.Drawing.Point(442, 963);
+            this.TestBox2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.TestBox2.Name = "TestBox2";
+            this.TestBox2.Size = new System.Drawing.Size(73, 85);
+            this.TestBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.TestBox2.TabIndex = 12;
+            this.TestBox2.TabStop = false;
+            // 
+            // Marken5
+            // 
+            this.Marken5.Image = global::Supermario.Properties.Resources.Marken;
+            this.Marken5.Location = new System.Drawing.Point(293, 1109);
+            this.Marken5.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Marken5.Name = "Marken5";
+            this.Marken5.Size = new System.Drawing.Size(73, 85);
+            this.Marken5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Marken5.TabIndex = 13;
+            this.Marken5.TabStop = false;
+            // 
+            // pictureBox6
+            // 
+            this.pictureBox6.Image = global::Supermario.Properties.Resources.Marken;
+            this.pictureBox6.Location = new System.Drawing.Point(558, 963);
+            this.pictureBox6.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.pictureBox6.Name = "pictureBox6";
+            this.pictureBox6.Size = new System.Drawing.Size(73, 85);
+            this.pictureBox6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox6.TabIndex = 14;
+            this.pictureBox6.TabStop = false;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.helpToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Padding = new System.Windows.Forms.Padding(7, 3, 0, 3);
+            this.menuStrip1.Size = new System.Drawing.Size(14402, 30);
+            this.menuStrip1.TabIndex = 15;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exitToolStripMenuItem});
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(55, 24);
+            this.helpToolStripMenuItem.Text = "Help";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(116, 26);
+            this.exitToolStripMenuItem.Text = "Exit";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
+            this.ClientSize = new System.Drawing.Size(1924, 1055);
+            this.Controls.Add(this.pictureBox6);
+            this.Controls.Add(this.Marken5);
+            this.Controls.Add(this.TestBox2);
+            this.Controls.Add(this.Mario);
+            this.Controls.Add(this.TestBox);
+            this.Controls.Add(this.Marken4);
+            this.Controls.Add(this.Marken3);
+            this.Controls.Add(this.Marken2);
+            this.Controls.Add(this.Marken1);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.Bakgrund);
+            this.Controls.Add(this.menuStrip1);
+            this.DoubleBuffered = true;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            ((System.ComponentModel.ISupportInitialize)(this.Bakgrund)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Marken1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Marken2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Marken3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Marken4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.TestBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Mario)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.TestBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Marken5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private PictureBox Bakgrund;
+        private PictureBox pictureBox1;
+        private PictureBox pictureBox2;
+        private PictureBox pictureBox3;
+        private PictureBox pictureBox4;
+        private PictureBox pictureBox5;
+        private PictureBox Marken1;
+        private PictureBox Marken2;
+        private PictureBox Marken3;
+        private PictureBox Marken4;
+        private PictureBox TestBox;
+        private System.Windows.Forms.Timer timer1;
+        private PictureBox Mario;
+        private PictureBox TestBox2;
+        private PictureBox Marken5;
+        private PictureBox pictureBox6;
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem helpToolStripMenuItem;
+        private ToolStripMenuItem exitToolStripMenuItem;
+    }
+}
