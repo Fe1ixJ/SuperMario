97e9eaa620e713836fb22f712d3f58ccfe9e4d1a
Fe1ixJ <66786588+Fe1ixJ@users.noreply.github.com>
2022-03-22 19:00:33 +0100

    Add files via upload

---
+++ MarioRemake/MarioRemake/Form2.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MarioRemake
+{
+    public partial class LoadingScreen : Form
+    {
+        public LoadingScreen()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            TestWorld frm1 = new TestWorld();
+            WorldSelector frm2 = new WorldSelector();
+            WorldSelector frm3 = new WorldSelector();
+            frm3.Show();
+            this.Hide();
+        }
+    }
+}
